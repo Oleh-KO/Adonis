85a96cb53a73f28ec0f477fa1194157ea3a28703
Oleh Kosach <oleh.kosach@wetelo.com>
2020-06-16 17:50:34 +0300

    building requests final

---
+++ server/app/Controllers/Http/ProjectController.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const  Project = use('App/Models/Project');
+const  AuthorizationService = use('App/Services/AuthorizationService');
+
+class ProjectController {
+  async index({ auth }) {
+    const user = await auth.getUser();
+    console.log(user)
+    return {
+      message:'hello'
+    }
+  }
+  async create({ auth, request }) {
+    const user = await auth.getUser();
+    const  { title } = request.all();
+    const project = new Project();
+    project.fill({
+      title,
+    })
+    await user.projects().save(project);
+  }
+  async destroy({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    await project.delete();
+    return project;
+  }
+  async update({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    project.merge(request.only('title'));
+    await project.save();
+    return project;
+
+  }
+}
+
+module.exports = ProjectController
